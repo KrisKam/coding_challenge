46594646c582bb7810838a26ef6fcb0dccef0295
Kris Kamikawa <kris.kamikawa@gmail.com>
2019-04-04 22:44:33 -0600

    remove console logs, add let to variables

--- app.js
+++ app.js
@@ -16,31 +16,32 @@ function nextGen(arr) {
   for (let i = 0; i < arr.length; i++) {
 
     let innerArr = [];
-    
+
     for (let j = 0; j < arr[i].length; j++) {
-      let cell1 = (i - 1) < 0 || (j - 1) < 0 ? 0 : arr[i - 1][j - 1],
-        cell2 = (i - 1) < 0 ? 0 : arr[i - 1][j],
-        cell3 = (i - 1) < 0 || (j + 1) === arr[i].length ? 0 : arr[i - 1][j + 1],
-        cell4 = (j - 1) < 0 ? 0 : arr[i][j - 1],
-        cell5 = (j + 1) === arr[i].length ? 0 : arr[i][j + 1],
-        cell6 = (i + 1) === arr.length || (j - 1) < 0 ? 0 : arr[i + 1][j - 1],
-        cell7 = (i + 1) === arr.length ? 0 : arr[i + 1][j],
-        cell8 = (i + 1) === arr.length || (j + 1) === arr[i].length ? 0 : arr[i + 1][j + 1];
+      
+      let cell1 = (i - 1) < 0 || (j - 1) < 0 ? 0 : arr[i - 1][j - 1];
+      let cell2 = (i - 1) < 0 ? 0 : arr[i - 1][j];
+      let cell3 = (i - 1) < 0 || (j + 1) === arr[i].length ? 0 : arr[i - 1][j + 1];
+      let cell4 = (j - 1) < 0 ? 0 : arr[i][j - 1];
+      let cell5 = (j + 1) === arr[i].length ? 0 : arr[i][j + 1];
+      let cell6 = (i + 1) === arr.length || (j - 1) < 0 ? 0 : arr[i + 1][j - 1];
+      let cell7 = (i + 1) === arr.length ? 0 : arr[i + 1][j];
+      let cell8 = (i + 1) === arr.length || (j + 1) === arr[i].length ? 0 : arr[i + 1][j + 1];
       
       let sum = cell1 + cell2 + cell3 + cell4 + cell5 + cell6 + cell7 + cell8;
 
-      console.log("i: ", i, " j: ", j, " cell value: ", arr[i][j], "=> ", cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, " sum: ", sum);
-
       if (sum === 3 || (sum === 2 && arr[i][j] === 1)) {
-        // newArr[i][j] = 1;
         innerArr.push(1);
       } else {
-        // newArr[i][j] = 0;
         innerArr.push(0);
       }
-    }
-    newArr.push(innerArr);
-  }
-  console.log(newArr);
 
+    }
+
+    newArr.push(innerArr);
+
+  }
+
+  console.log(newArr);
+  return newArr;
 }
--- test/test.js
+++ test/test.js
@@ -6,4 +6,8 @@ describe('Application', () => {
   it('returns desired result', () => {
     assert.deepEqual(application(arguments), 'desired result')
   })
+
+  it('returns a next generation array', () => {
+    assert.isArray(application([[0,1,0], [1,0,1], [1,1,0]], 'is an array'))
+  })
 });
